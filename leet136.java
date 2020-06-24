a420697d5977fcdfb679bf9740ae0b6a8d35a8b8
Krishanu Shekhar <41140227+krishanuskr@users.noreply.github.com>
2020-06-24 16:32:33 +0530

    Add files via upload

---
+++ leet136.java
@@ -0,0 +1,31 @@
+/*Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+        Note:
+
+        Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+        Example 1:
+
+        Input: [2,2,1]
+        Output: 1
+        Example 2:
+
+        Input: [4,1,2,1,2]
+        Output: 4*/
+public class leet136 {
+    public static int singleNumber(int[] nums) {
+       //BITWISE XOR (^). THIS CODE WORKS ONLY AS LONG AS ONLY ALL NUMBERS EXCEPT 1 ARE REPEATED EVEN NUMBER OF TIMES
+       int res=0;
+       for(int i:nums)
+       {
+           res ^= i;
+           System.out.println("res at "+i+" = " +res+"\n");
+       }
+       return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={4,1,2,1,2,6,6,2,2,1,1,6,6,6,6};
+        System.out.println(singleNumber(nums));
+    }
+}
